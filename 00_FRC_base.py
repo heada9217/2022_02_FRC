d5106306aa413f3e2edcd262138e8f93f74b63c6
heada9217 <99290797+heada9217@users.noreply.github.com>
2022-04-12 10:14:56 +1200

    Add files via upload

---
+++ 00_FRC_base.py
@@ -0,0 +1,50 @@
+# import libraries
+
+
+# *** Functions go here ***
+
+#checks that input is either a float orr an 
+# integer that is more than zero. Takes in custom error message.
+def num_check(question, error, num_type):
+    
+    error = "Please enter a whole number greater than zero"
+
+
+    valid = False
+    while not valid:
+
+        try: 
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+        
+
+        except ValueError:
+            print(error)
+
+
+#Checks that user has entered yes / no to a question 
+def yes_no(question):
+
+
+
+    to_check = ["yes", "no"]
+
+    valid = False 
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item:
+                return var_item
+        
+        print("Please enter either yes or no...\n")
+
+
+# *** Main Routine goes here *** 
---
+++ 01_numcheck_v1.py
@@ -0,0 +1,29 @@
+#Main routine goes here
+
+
+def num_check(question, error, num_type):
+    
+    error = "Please enter a whole number greater than zero"
+
+
+    valid = False
+    while not valid:
+
+        try: 
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+        
+
+        except ValueError:
+            print(error)
+
+
+get_int = num_check("how many?: ", 'Please enter a whole number larger than zero.', int)
+
+get_cost = num_check("How much does it cost?: $", "Please enter a whole number larger than zero.", float)
+
+print("You need: {} /n It costs: ${}").format(get_int,get_cost)
